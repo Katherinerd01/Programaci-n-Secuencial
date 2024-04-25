a00e1b63fff723ae2fa0d4ef8bc743f1640d7659
Katherinerd01 <katherinerdd01@gmail.com>
2024-04-25 19:15:09 -0400

    Practica en C#

---
+++ Practica C#/ProgramacionSecuencial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero1, numero2;
+
+        Console.WriteLine("Ingrese el primer número entero:");
+        numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Ingrese el segundo número entero:");
+        numero2 = Convert.ToInt32(Console.ReadLine());
+
+        int suma = numero1 + numero2;
+        int producto = numero1 * numero2;
+
+        Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
+        Console.WriteLine($"El producto de {numero1} y {numero2} es: {producto}");
+
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
+
+
+
+
