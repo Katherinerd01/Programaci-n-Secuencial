0ba423da9f7d7122160de087e4b41dff5c10628a
Katherine Roque <82182869+Katherinerd01@users.noreply.github.com>
2024-04-30 21:32:52 -0400

    ProgramacionSecuencial.cs

--- Practica C#/ProgramacionSecuencial.cs
+++ Practica C#/ProgramacionSecuencial.cs
@@ -1,26 +1,42 @@
 ﻿using System;
 
+namespace ProgramacionSecuencial;
 class Program
 {
     static void Main(string[] args)
     {
+
+
         int numero1, numero2;
 
-        Console.WriteLine("Ingrese el primer número entero:");
-        numero1 = Convert.ToInt32(Console.ReadLine());
+        try
+        {
 
-        Console.WriteLine("Ingrese el segundo número entero:");
-        numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el primer número entero:");
+            numero1 = Convert.ToInt32(Console.ReadLine());
 
-        int suma = numero1 + numero2;
-        int producto = numero1 * numero2;
+            Console.WriteLine("Ingrese el segundo número entero:");
+            numero2 = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
-        Console.WriteLine($"El producto de {numero1} y {numero2} es: {producto}");
+            int suma = numero1 + numero2;
+            int producto = numero1 * numero2;
 
-        Console.ReadLine();
-    }
-}
+            Console.WriteLine($"La suma de {numero1} y {numero2} es: {suma}");
+            Console.WriteLine($"El producto de {numero1} y {numero2} es: {producto}");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocurrio el Siguiente error: { ex.Message }");
+        }
+                Console.ReadLine();
+            }
+
+
+        }
+
+
+
 
 
 
